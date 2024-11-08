1886e715d3f2c3dd0c4c37f9f67daa77d72d4be5
Kwasi Adabo Boakye <Kwasadaboboakye@gmail.com>
2024-11-08 13:58:35 +0000

    removed users

--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,12 @@
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../swagger');
-const users = require('../routes/users')
-const staff = require('../routes/staff')
-const facility = require('../routes/facility')
-const packages = require('../routes/package')
-const institutions = require('../routes/institution')
-const smsFetch =require('../routes/smsFetch')
+const staff = require('../routes/staff');
+const facility = require('../routes/facility');
+const packages = require('../routes/package');
+const institutions = require('../routes/institution');
+const smsFetch = require('../routes/smsFetch');
 // const unitHead = require('../routes/unitHead')
-const menus = require('../routes/menus')
+const menus = require('../routes/menus');
 // const staff = require('../routes/staff')
 // const bank = require('../routes/bank')
 // const attendance = require('../routes/attendance')
@@ -19,8 +18,8 @@ const menus = require('../routes/menus')
 // const salarySetup = require('../routes/salarysetup')
 // const specialty = require('../routes/specialty')
 // const employee = require('../routes/employeeDetails')
-const Auth = require('../routes/auth')
-const LeaveAuth = require('../routes/leaveauth')
+const Auth = require('../routes/auth');
+const LeaveAuth = require('../routes/leaveauth');
 // const nationality = require('../routes/nationality')
 // const directorate = require('../routes/directorate')
 // const department = require('../routes/department')
@@ -29,47 +28,47 @@ const LeaveAuth = require('../routes/leaveauth')
 // const appointment = require('../routes/appointment')
 // const currentPost = require('../routes/currentPost')
 // const unit = require('../routes/unit')
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 //const app = express()
 // const router = express.Router();
 
-const apiPrefix = 'api'
+const apiPrefix = 'api';
 
 module.exports = function (app) {
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.json())
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-  //app.use(cors())
-  // app.use('/' + apiPrefix + '/users', users)
-  app.use('/' + apiPrefix + '/menus', menus)
-  // app.use('/api/staff', staff)
-  // app.use('/api/nationality', nationality)
-  // app.use('/api/users', users)
-  app.use('/api/staff', staff)
-  // app.use('/api/leaveplan', leavePlan)
-  // app.use('/api/auth', Auth)
-  app.use('/api/auth', Auth)
-  app.use('/api/facilities', facility)
-  app.use('/api/packages', packages)
-  app.use('/api/institutions', institutions)
-  app.use('/api/smsfetch', smsFetch)
-  // app.use('/api/unit', unit)
-  // app.use('/api/leaveType', leaveType)
-  // app.use('/api/employee', employee)
-  // app.use('/api/grade', grade)
-  // app.use('/api/category', category)
-  // app.use('/api/salarysetup', salarySetup)
-  // app.use('/api/specialty', specialty)
-  // app.use('/api/appointment', appointment)
-  // app.use('/api/currentPost', currentPost)
-  // app.use('/api/holidays', holidays)
-  // app.use('/api/leaveyear', leaveYear)
-  // app.use('/api/supervisor', unitSupervisor)
-  // app.use('/api/planyear', planYear)
-  // app.use('/api/depthead', deptHead)
-  // app.use('/api/unithead', unitHead)
-  // app.use('/api/attendance', attendance)
-  // app.use('/api/leaverequest', leaveRequest)
-}
+	//app.use(cors())
+	// app.use('/' + apiPrefix + '/users', users)
+	app.use('/' + apiPrefix + '/menus', menus);
+	// app.use('/api/staff', staff)
+	// app.use('/api/nationality', nationality)
+	// app.use('/api/users', users)
+	app.use('/api/staff', staff);
+	// app.use('/api/leaveplan', leavePlan)
+	// app.use('/api/auth', Auth)
+	app.use('/api/auth', Auth);
+	app.use('/api/facilities', facility);
+	app.use('/api/packages', packages);
+	app.use('/api/institutions', institutions);
+	app.use('/api/smsfetch', smsFetch);
+	// app.use('/api/unit', unit)
+	// app.use('/api/leaveType', leaveType)
+	// app.use('/api/employee', employee)
+	// app.use('/api/grade', grade)
+	// app.use('/api/category', category)
+	// app.use('/api/salarysetup', salarySetup)
+	// app.use('/api/specialty', specialty)
+	// app.use('/api/appointment', appointment)
+	// app.use('/api/currentPost', currentPost)
+	// app.use('/api/holidays', holidays)
+	// app.use('/api/leaveyear', leaveYear)
+	// app.use('/api/supervisor', unitSupervisor)
+	// app.use('/api/planyear', planYear)
+	// app.use('/api/depthead', deptHead)
+	// app.use('/api/unithead', unitHead)
+	// app.use('/api/attendance', attendance)
+	// app.use('/api/leaverequest', leaveRequest)
+};
