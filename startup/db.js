697c1ec1254f7f07a517ddde6b0e00cff0c793f1
Kwasi Adabo Boakye <Kwasadaboboakye@gmail.com>
2025-01-15 09:03:27 +0000

    Server name Changed

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,11 @@
 const sql = require('mssql');
 
 const db = {
-	server: 'SQL5088.site4now.net',
+	server: 'SQL8011.site4now.net',
 	//server: '192.168.8.127',
 	port: 1433,
 	user: 'db_aa95d7_schoolsmsdb_admin',
-	password: 'Kaw3se4dr5$$1',
+	password: 'Kaw3se4dr5',
 	database: 'db_aa95d7_schoolsmsdb',
 	options: {
 		encrypt: false,
